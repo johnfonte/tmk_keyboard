214ff78eb36caceb91446dc9d7bf40463b8c7168
John Fonte <john@fonte.com>
2015-02-05 13:34:12 -0500

    adding virulent firmware
    based off phantom

---
+++ keyboard/virulent/keymap.c
@@ -0,0 +1,115 @@
+/*
+Copyright 2011 Jun Wako <wakojun@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/* 
+ * Keymap for Virulent controller
+ */
+#include <stdint.h>
+#include <stdbool.h>
+#include <avr/pgmspace.h>
+#include "keycode.h"
+#include "action.h"
+#include "action_macro.h"
+#include "report.h"
+#include "host.h"
+#include "debug.h"
+#include "keymap.h"
+
+// Convert physical keyboard layout to matrix array.
+// This is a macro to define keymap easily in keyboard layout form.
+#define KEYMAP( \
+                             K5F, K5G, K5H, K5I, K5J, K5K, K5L, K5M, K5N, K5O, K5P, K5Q, K5R, K5S, \
+    K4A, K4B, K4C, K4D, K4E, K4F, K4G, K4H, K4I, K4J, K4K, K4L, K4M, K4N, K4O, K4P, K4Q, K4R, K4S, \
+    K3A, K3B, K3C, K3D, K3E, K3F, K3G, K3H, K3I, K3J, K3K, K3L, K3M, K3N, K3O, K3P, K3Q, K3R, K3S, \
+    K2A, K2B, K2C, K2D, K2E, K2F, K2G, K2H, K2I, K2J, K2K, K2L, K2M, K2N, K2O, K2P, K2Q,      K2S, \
+    K1A, K1B, K1C, K1D, K1E, K1F, K1G, K1H, K1I, K1J, K1K, K1L, K1M, K1N, K1O, K1P, K1Q, K1R, K1S, \
+    K0A, K0B, K0C, K0D, K0E, K0F, K0G,      K0I, K0J, K0K, K0L, K0M, K0N,      K0P, K0Q, K0R, K0S  \
+) { \
+/*             0         1         2         3         4         5         6         7         8         9         10        11        12        13        14        15        16        17   */  \
+/* 5 */   { KC_##K4A, KC_##K4B, KC_##K4C, KC_##K4D, KC_##K5F, KC_##K5G, KC_##K5H, KC_##K5I, KC_##K5J, KC_##K5K, KC_##K5L, KC_##K5M, KC_##K5N, KC_##K5O, KC_##K5P, KC_##K5Q, KC_##K5R, KC_##K5S}, \
+/* 4 */   { KC_##K3A, KC_##K3B, KC_##K3C, KC_##K4E, KC_##K4F, KC_##K4G, KC_##K4H, KC_##K4I, KC_##K4J, KC_##K4K, KC_##K4L, KC_##K4M, KC_##K4N, KC_##K4O, KC_##K4P, KC_##K4Q, KC_##K4R, KC_##K4S}, \
+/* 3 */   { KC_##K2A, KC_##K2B, KC_##K3D, KC_##K3E, KC_##K3F, KC_##K3G, KC_##K3H, KC_##K3I, KC_##K3J, KC_##K3K, KC_##K3L, KC_##K3M, KC_##K3N, KC_##K3O, KC_##K3P, KC_##K3Q, KC_##K3R, KC_##K3S}, \
+/* 2 */   { KC_##K1A, KC_##K2C, KC_##K2D, KC_##K2E, KC_##K2F, KC_##K2G, KC_##K2H, KC_##K2I, KC_##K2J, KC_##K2K, KC_##K2L, KC_##K2M, KC_##K2N, KC_##K2O, KC_##K2P, KC_##K2Q, KC_NO,  , KC_##K2S}, \
+/* 1 */   { KC_##K1B, KC_##K1C, KC_##K1D, KC_##K1E, KC_##K1F, KC_##K1G, KC_##K1H, KC_##K1I, KC_##K1J, KC_##K1K, KC_##K1L, KC_##K1M, KC_##K1N, KC_##K1O, KC_##K1P, KC_##K1Q, KC_##K1R, KC_##K1S}, \
+/* 0 */   { KC_##K0A, KC_##K0B, KC_##K0C, KC_##K0D, KC_##K0E, KC_##K0F, KC_##K0G, KC_##K0I, KC_##K0J, KC_##K0K, KC_##K0L, KC_##K0M, KC_##K0N, KC_NO,    KC_##K0P, KC_##K0Q, KC_##K0R, KC_##K0S} \
+}
+
+#define KEYMAP_ANSI_TK( \
+                             K5F,      K5H, K5I, K5J, K5K, K5L, K5M, K5N, K5O, K5P, K5Q, K5R, K5S, \
+         K4B, K4C, K4D, K4E, K4F, K4G, K4H, K4I, K4J, K4K, K4L, K4M, K4N, K4O, K4P, K4Q, K4R, K4S, \
+         K3B, K3C, K3D, K3E, K3F, K3G, K3H, K3I, K3J, K3K, K3L, K3M, K3N, K3O, K3P, K3Q, K3R, K3S, \
+         K2B, K2C, K2D,      K2F, K2G, K2H, K2I, K2J, K2K, K2L, K2M, K2N, K2O, K2P, K2Q,      K2S, \
+         K1B, K1C, K1D, K1E,      K1G, K1H, K1I, K1J, K1K, K1L, K1M, K1N, K1O, K1P, K1Q,      K1S, \
+              K0C, K0D,      K0F, K0G,      K0I, K0J, K0K, K0L, K0M, K0N,      K0P, K0Q, K0R, K0S  \
+) KEYMAP( \
+                             K5F, NO,  K5H, K5I, K5J, K5K, K5L, K5M, K5N, K5O, K5P, K5Q, K5R, K5S, \
+    NO,  K4B, K4C, K4D, K4E, K4F, K4G, K4H, K4I, K4J, K4K, K4L, K4M, K4N, K4O, K4P, K4Q, K4R, K4S, \
+    NO,  K3B, K3C, K3D, K3E, K3F, K3G, K3H, K3I, K3J, K3K, K3L, K3M, K3N, K3O, K3P, K3Q, K3R, K3S, \
+    NO,  K2B, K2C, K2D, NO,  K2F, K2G, K2H, K2I, K2J, K2K, K2L, K2M, K2N, K2O, K2P, K2Q,      K2S, \
+    NO,  K1B, K1C, K1D, K1E, NO,  K1G, K1H, K1I, K1J, K1K, K1L, K1M, K1N, K1O, K1P, K1Q, NO,  K1S, \
+    NO,  NO,  K0C, K0D, NO,  K0F, K0G,      K0I, K0J, K0K, K0L, K0M, K0N,      K0P, K0Q, K0R, K0S  \
+)
+
+#define KEYMAP_ANSI_TKL( \
+                             K5F,      K5H, K5I, K5J, K5K, K5L, K5M, K5N, K5O, K5P, K5Q, K5R, K5S, \
+              K4C, K4D, K4E, K4F, K4G, K4H, K4I, K4J, K4K, K4L, K4M, K4N, K4O, K4P, K4Q, K4R, K4S, \
+              K3C, K3D, K3E, K3F, K3G, K3H, K3I, K3J, K3K, K3L, K3M, K3N, K3O, K3P, K3Q, K3R, K3S, \
+              K2C, K2D, K2E, K2F, K2G, K2H, K2I, K2J, K2K, K2L, K2M, K2N, K2O, K2P, K2Q,      K2S, \
+                   K1D,           K1G, K1H, K1I, K1J, K1K, K1L, K1M, K1N, K1O, K1P, K1Q,      K1S, \
+              K0C, K0D, K0E, K0F, K0G,      K0I, K0J, K0K, K0L, K0M, K0N,      K0P, K0Q, K0R, K0S  \
+) KEYMAP( \
+                             K5F, NO,  K5H, K5I, K5J, K5K, K5L, K5M, K5N, K5O, K5P, K5Q, K5R, K5S, \
+    NO,  NO,  K4C, K4D, K4E, K4F, K4G, K4H, K4I, K4J, K4K, K4L, K4M, K4N, K4O, K4P, K4Q, K4R, K4S, \
+    NO,  NO,  K3C, K3D, K3E, K3F, K3G, K3H, K3I, K3J, K3K, K3L, K3M, K3N, K3O, K3P, K3Q, K3R, K3S, \
+    NO,  NO,  K2C, K2D, K2E, K2F, K2G, K2H, K2I, K2J, K2K, K2L, K2M, K2N, K2O, K2P, K2Q,      K2S, \
+    NO,  NO,  NO,  K1D, NO,  NO,  K1G, K1H, K1I, K1J, K1K, K1L, K1M, K1N, K1O, K1P, K1Q, NO,  K1S, \
+    NO,  NO,  K0C, K0D, K0E, K0F, K0G,      K0I, K0J, K0K, K0L, K0M, K0N,      K0P, K0Q, K0R, K0S  \
+)
+
+#if defined(LAYOUT_ALLKEYS)
+    #include "keymap_allkeys.h"
+#elif defined(LAYOUT_ANSI_TK)
+    #include "keymap_ansi_tk.h"
+#else
+    #include "keymap_ansi_tkl.h"
+#endif
+
+#define KEYMAPS_SIZE    (sizeof(keymaps) / sizeof(keymaps[0]))
+#define FN_ACTIONS_SIZE (sizeof(fn_actions) / sizeof(fn_actions[0]))
+
+/* translates key to keycode */
+uint8_t keymap_key_to_keycode(uint8_t layer, keypos_t key)
+{
+    if (layer < KEYMAPS_SIZE) {
+        return pgm_read_byte(&keymaps[(layer)][(key.row)][(key.col)]);
+    } else {
+        // fall back to layer 0
+        return pgm_read_byte(&keymaps[0][(key.row)][(key.col)]);
+    }
+}
+
+/* translates Fn keycode to action */
+action_t keymap_fn_to_action(uint8_t keycode)
+{
+    action_t action;
+    if (FN_INDEX(keycode) < FN_ACTIONS_SIZE) {
+        action.code = pgm_read_word(&fn_actions[FN_INDEX(keycode)]);
+    } else {
+        action.code = ACTION_NO;
+    }
+    return action;
+}
---
+++ keyboard/virulent/matrix.c
@@ -0,0 +1,286 @@
+/* Copyright 2012 Jun Wako <wakojun@gmail.com>
+ *
+ * This is heavily based on phantom/board.{c|h}.
+ * https://github.com/BathroomEpiphanies/AVR-Keyboard
+ *
+ * Copyright (c) 2012 Fredrik Atmer, Bathroom Epiphanies Inc
+ * http://bathroomepiphanies.com
+ *
+ * As for liscensing consult with the original files or its author.
+ */
+#include <stdint.h>
+#include <stdbool.h>
+#include <avr/io.h>
+#include <util/delay.h>
+#include "print.h"
+#include "debug.h"
+#include "util.h"
+#include "matrix.h"
+
+
+#ifndef DEBOUNCE
+#   define DEBOUNCE	0
+#endif
+static uint8_t debouncing = DEBOUNCE;
+
+// bit array of key state(1:on, 0:off)
+static matrix_row_t matrix[MATRIX_ROWS];
+static matrix_row_t matrix_debouncing[MATRIX_ROWS];
+
+static uint8_t read_rows(void);
+static void init_rows(void);
+static void unselect_cols(void);
+static void select_col(uint8_t col);
+
+#ifndef SLEEP_LED_ENABLE
+/* LEDs are on output compare pins OC1B OC1C
+   This activates fast PWM mode on them.
+   Prescaler 256 and 8-bit counter results in
+   16000000/256/256 = 244 Hz blink frequency.
+   LED_A: Caps Lock
+   LED_B: Scroll Lock  */
+/* Output on PWM pins are turned off when the timer 
+   reaches the value in the output compare register,
+   and are turned on when it reaches TOP (=256). */
+static
+void setup_leds(void)
+{
+    TCCR1A |=      // Timer control register 1A
+        (1<<WGM10) | // Fast PWM 8-bit
+        (1<<COM1B1)| // Clear OC1B on match, set at TOP
+        (1<<COM1C1); // Clear OC1C on match, set at TOP
+    TCCR1B |=      // Timer control register 1B
+        (1<<WGM12) | // Fast PWM 8-bit
+        (1<<CS12);   // Prescaler 256
+    OCR1B = LED_BRIGHTNESS;    // Output compare register 1B
+    OCR1C = LED_BRIGHTNESS;    // Output compare register 1C
+    // LEDs: LED_A -> PORTB6, LED_B -> PORTB7
+    DDRB  |= (1<<6) | (1<<7);
+    PORTB  &= ~((1<<6) | (1<<7));
+}
+#endif
+
+inline
+uint8_t matrix_rows(void)
+{
+    return MATRIX_ROWS;
+}
+
+inline
+uint8_t matrix_cols(void)
+{
+    return MATRIX_COLS;
+}
+
+void matrix_init(void)
+{
+    // To use PORTF disable JTAG with writing JTD bit twice within four cycles.
+    MCUCR |= (1<<JTD);
+    MCUCR |= (1<<JTD);
+	
+    // initialize row and col
+    unselect_cols();
+    init_rows();
+#ifndef SLEEP_LED_ENABLE
+    setup_leds();
+#endif
+
+    // initialize matrix state: all keys off
+    for (uint8_t i = 0; i < MATRIX_ROWS; i++)  {
+        matrix[i] = 0;
+        matrix_debouncing[i] = 0;
+    }
+}
+
+uint8_t matrix_scan(void)
+{
+    for (uint8_t col = 0; col < MATRIX_COLS; col++) {  // 0-16
+        select_col(col);
+        _delay_us(3);       // without this wait it won't read stable value.
+        uint8_t rows = read_rows();
+        for (uint8_t row = 0; row < MATRIX_ROWS; row++) {  // 0-5
+            bool prev_bit = matrix_debouncing[row] & ((matrix_row_t)1<<col);
+            bool curr_bit = rows & (1<<row);
+            if (prev_bit != curr_bit) {
+                matrix_debouncing[row] ^= ((matrix_row_t)1<<col);
+                if (debouncing) {
+                    dprint("bounce!: "); dprintf("%02X", debouncing); dprintln();
+                }
+                debouncing = DEBOUNCE;
+            }
+        }
+        unselect_cols();
+    }
+
+    if (debouncing) {
+        if (--debouncing) {
+            _delay_ms(1);
+        } else {
+            for (uint8_t i = 0; i < MATRIX_ROWS; i++) {
+                matrix[i] = matrix_debouncing[i];
+            }
+        }
+    }
+
+    return 1;
+}
+
+bool matrix_is_modified(void)
+{
+    if (debouncing) return false;
+    return true;
+}
+
+inline
+bool matrix_is_on(uint8_t row, uint8_t col)
+{
+    return (matrix[row] & ((matrix_row_t)1<<col));
+}
+
+inline
+matrix_row_t matrix_get_row(uint8_t row)
+{
+    return matrix[row];
+}
+
+void matrix_print(void)
+{
+    print("\nr/c 0123456789ABCDEF\n");
+    for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
+        xprintf("%02X: %032lb\n", row, bitrev32(matrix_get_row(row)));
+    }
+}
+
+uint8_t matrix_key_count(void)
+{
+    uint8_t count = 0;
+    for (uint8_t i = 0; i < MATRIX_ROWS; i++) {
+        count += bitpop32(matrix[i]);
+    }
+    return count;
+}
+
+/* Row pin configuration
+ * row: 0   1   2   3   4   5
+ * pin: D6  D7  E0  E1  C0  C1
+ */
+static void init_rows(void)
+{
+    // Input with pull-up(DDR:0, PORT:1)
+    DDRD  &= ~0b11000000;
+    PORTD |= 0b11000000;
+    DDRE  &= ~0b00000011;
+    PORTE |= 0b00000011;
+    DDRC  &= ~0b00000011;
+    PORTC |= 0b00000011;
+}
+
+static uint8_t read_rows(void)
+{
+    return (PIND&(1<<6) ? 0 : (1<<0)) |
+           (PIND&(1<<7) ? 0 : (1<<1)) |
+           (PINE&(1<<0) ? 0 : (1<<2)) |
+           (PINE&(1<<1) ? 0 : (1<<3)) |
+           (PINC&(1<<0) ? 0 : (1<<4)) |
+           (PINC&(1<<1) ? 0 : (1<<5));
+}
+
+/* Column pin configuration
+ * col: 0   1   2   3   4   5   6   7   8   9   10  11  12  13  14  15  16  17  18
+ * pin: B2  B1  B0  D4  D5  C2  C3  C4  C5  C6  C7  F0  F1  F2  F3  F4  F5  F6  F7
+ */
+static void unselect_cols(void)
+{
+    // Hi-Z(DDR:0, PORT:0) to unselect
+    DDRC  |= 0b11111100; // PC: 7 6 5 4 3 2
+    PORTC |= 0b11111100;
+    DDRD  |= 0b00110000; //PD: 5 4
+    PORTD |= 0b00110000;
+    // DDRE  |= 0b01000000; // PE: x
+    // PORTE |= 0b01000000;
+    DDRF  |= 0b11111111; // PF: 7 6 5 4 3 2 1 0
+    PORTF |= 0b11111111;
+}
+
+static void select_col(uint8_t col)
+{
+    // Output low(DDR:1, PORT:0) to select
+    switch (col) {
+        case 0:
+            DDRB  |= (1<<2);
+            PORTB &= ~(1<<2);
+            break;
+        case 1:
+            DDRB  |= (1<<1);
+            PORTB &= ~(1<<1);
+            break;
+        case 2:
+            DDRB  |= (1<<0);
+            PORTB &= ~(1<<0);
+            break;
+        case 3:
+            DDRD  |= (1<<4);
+            PORTD &= ~(1<<4);
+            break;
+        case 4:
+            DDRD  |= (1<<5);
+            PORTD &= ~(1<<5);
+            break;
+        case 5:
+            DDRC  |= (1<<2);
+            PORTC &= ~(1<<2);
+            break;
+        case 6:
+            DDRC  |= (1<<3);
+            PORTC &= ~(1<<3);
+            break;
+        case 7:
+            DDRC  |= (1<<4);
+            PORTC &= ~(1<<4);
+            break;
+        case 8:
+            DDRC  |= (1<<5);
+            PORTC &= ~(1<<5);
+            break;
+        case 9:
+            DDRC  |= (1<<6);
+            PORTC &= ~(1<<6);
+            break;
+        case 10:
+            DDRC  |= (1<<7);
+            PORTC &= ~(1<<7);
+            break;
+        case 11:
+            DDRF  |= (1<<0);
+            PORTF &= ~(1<<0);
+            break;
+        case 12:
+            DDRF  |= (1<<1);
+            PORTF &= ~(1<<1);
+            break;
+        case 13:
+            DDRF  |= (1<<2);
+            PORTF &= ~(1<<2);
+            break;
+        case 14:
+            DDRF  |= (1<<3);
+            PORTF &= ~(1<<3);
+            break;
+        case 15:
+            DDRF  |= (1<<4);
+            PORTF &= ~(1<<4);
+            break;
+        case 16:
+            DDRF  |= (1<<5);
+            PORTF &= ~(1<<5);
+            break;
+        case 17:
+            DDRF  |= (1<<6);
+            PORTF &= ~(1<<6);
+            break;
+        case 18:
+            DDRF  |= (1<<7);
+            PORTF &= ~(1<<7);
+            break;
+    }
+}
